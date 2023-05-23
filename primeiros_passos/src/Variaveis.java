b7e9618f1168de15c271385aaa3ad2f1cd7b4e88
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-05-22 21:36:26 -0300

    feat: tipos primitivos de variaveis em Java

---
+++ primeiros_passos/src/Variaveis.java
@@ -0,0 +1,16 @@
+public class Variaveis {
+    public static void main(String[] args){
+        // Tipos de variáveis
+        byte numeroByte;                // Referente à int  8 bits
+        short numeroShort;              // Referente à int  16 bits
+        int numeroInt;                  // Referemte à int 32 bits
+        long numeroLong;                // Referente à int  64 bits
+
+        float numeroFloat;              // Referente à float 32 bits
+        double numeroDouble;            // Referente à float 64 bits
+
+        char valorChar;                 // Referente à um caracter de 16 bits
+        boolean valorBoolean;           // Referente à false/true e possui 1 bit
+
+    }
+}
