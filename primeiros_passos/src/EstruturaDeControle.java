cf057ba86dfbc514457deab7e06cbb8f94790ada
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-05-26 18:53:50 -0300

    fix: correção switch-case

--- primeiros_passos/src/EstruturaDeControle.java
+++ primeiros_passos/src/EstruturaDeControle.java
@@ -21,23 +21,32 @@ public class EstruturaDeControle {
         switch (diaDaSemana){
             case 1:
                 System.out.println("Domingo");
+                break;
             case 2:
                 System.out.println("Segunda-feira");
+                break;
             case 3:
                 System.out.println("Terça-feira");
+                break;
             case 4:
                 System.out.println("Quarta-feira");
+                break;
             case 5:
                 System.out.println("Quinta-feira");
+                break;
             case 6:
                 System.out.println("Sexta-feira");
+                break;
             case 7:
                 System.out.println("Sábado");
+                break;
             default:
                 System.out.println("Número inválido");
+                break;
         }
 
         // ternário
         String ternario = (num != 10) ? "Os numeros são diferentes" : "Os números são iguais";
+        System.out.println(ternario);
     }
 }
