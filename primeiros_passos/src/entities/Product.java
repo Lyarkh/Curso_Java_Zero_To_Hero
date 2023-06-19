d67d7c3c9db1138eacbc7bdd15b878ec5b145dcb
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-06-19 20:50:08 -0300

    feat/refact: adicionando format nos numeros float e arrendondando para duas casas decimais no método toString

--- primeiros_passos/src/entities/Product.java
+++ primeiros_passos/src/entities/Product.java
@@ -19,6 +19,9 @@ public class Product {
     }
 
     public String toString() {
-        return this.name + ", $" + this.price + ", " +  this.quantity + " units, Total: $" + this.totalValueInStock();
+        return this.name + ", $" + String.format("%.2f", this.price)
+                + ", " +  this.quantity
+                + " units, Total: $"
+                + String.format("%.2f", this.totalValueInStock());
     }
 }
